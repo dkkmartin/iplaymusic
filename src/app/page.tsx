c5812f5201f8c0ba853236073f4d436740576e37
Martin <martinbruun1995@gmail.com>
2024-05-07 13:50:41 +0200

    chore: Update categories page and index component with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,49 @@
 import GradientText from '@/components/text/gradientHeading'
-import PageContent from '@/components/text/pages/pageContent'
-import Image from 'next/image'
+import PageContent from '@/components/pages/pageContent'
+import ImageCardWithOverlay from '@/components/cards/ImageCardWithOverlay'
+
+const mockup = [
+	{
+		src: '/stock.jpg',
+		alt: 'Placeholder',
+		text: 'The greatest showman',
+		sub_text: 'Soundtrack',
+	},
+	{
+		src: '/stock.jpg',
+		alt: 'Placeholder',
+		text: 'The greatest showman',
+		sub_text: 'Soundtrack',
+	},
+	{
+		src: '/stock.jpg',
+		alt: 'Placeholder',
+		text: 'The greatest showman',
+		sub_text: 'Soundtrack',
+	},
+	{
+		src: '/stock.jpg',
+		alt: 'Placeholder',
+		text: 'The greatest showman',
+		sub_text: 'Soundtrack',
+	},
+]
 
 export default function Home() {
 	return (
-		<PageContent className="">
+		<PageContent>
 			<GradientText>Featured</GradientText>
-			<Image
-				className="rounded-md"
-				src={'https://placehold.co/325x425'}
-				width={325}
-				height={425}
-				alt="Placeholder"
-			></Image>
+			<div className="flex flex-col items-center gap-8">
+				{mockup.map((item, index) => (
+					<ImageCardWithOverlay
+						key={index}
+						imgSrc={item.src}
+						imageAlt={item.alt}
+						text={item.text}
+						subText={item.sub_text}
+					></ImageCardWithOverlay>
+				))}
+			</div>
 		</PageContent>
 	)
 }
