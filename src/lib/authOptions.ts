d1e84463b7d34c9b4b4ea447afd421bd4ca84dae
Martin <martinbruun1995@gmail.com>
2024-05-09 09:51:40 +0200

    refactor: Update authOptions to handle token expiration and refresh
    This commit refactors the `authOptions` module to handle token expiration and refresh. It adds logic to check if the token has expired and, if so, makes a request to the Spotify API to refresh the token. The refreshed token is then stored in the `token` object. This ensures that the user remains authenticated even if their token expires. Additionally, the commit updates the `session` callback to assign the refreshed access token to the `session.user.token` property.

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -15,14 +15,49 @@ export const authOptions: NextAuthOptions = {
 	callbacks: {
 		async jwt({ token, account }) {
 			if (account) {
-				token = Object.assign({}, token, { access_token: account.access_token })
+				token = Object.assign({}, token, {
+					access_token: account.access_token,
+					refresh_token: account.refresh_token, // store the refresh_token
+					expires_in: account.expires_in, // store the expiry time
+					token_received_at: Date.now(), // store the time when the token was received
+				})
 			}
+
+			// check if the token has expired
+			if (
+				token.expires_in &&
+				typeof token.token_received_at === 'number' &&
+				Date.now() - token.token_received_at > Number(token.expires_in) * 1000
+			) {
+				const res = await fetch('https://accounts.spotify.com/api/token', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+					body: new URLSearchParams({
+						grant_type: 'refresh_token',
+						refresh_token: token.refresh_token as string, // ensure refresh_token is of type string
+						client_id: process.env.CLIENT_ID as string,
+						client_secret: process.env.CLIENT_SECRET as string,
+					}),
+				})
+
+				const data = await res.json()
+
+				if (data.error) {
+					console.error(data.error)
+				} else {
+					token = Object.assign({}, token, {
+						access_token: data.access_token,
+						expires_in: data.expires_in,
+						token_received_at: Date.now(),
+					})
+				}
+			}
+
 			return token
 		},
 		async session({ session, token }) {
 			if (session) {
-				session = Object.assign({}, session, { access_token: token.access_token })
-				console.log(session)
+				session.user = Object.assign({}, session.user, { token: token.access_token })
 			}
 			return session
 		},
