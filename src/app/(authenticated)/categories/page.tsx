89244c657dc8531fed8c78c49cd220502279ace9
Martin <martinbruun1995@gmail.com>
2024-05-09 23:27:49 +0200

    refactor: Update categories page to use CategoriesAccordion component

--- src/app/(authenticated)/categories/page.tsx
+++ src/app/(authenticated)/categories/page.tsx
@@ -1,186 +1,59 @@
 import PageContent from '@/components/pages/pageContent'
 import GradientText from '@/components/text/gradientHeading'
-import CombinedAccordion from '@/components/ui/combinedAccordion'
+import CategoriesAccordion from '@/components/ui/categoriesAccordion'
+import { authOptions } from '@/lib/authOptions'
 import { spotifyFetch } from '@/lib/utils'
-import { type Root } from '@/types/categories/categories'
 import { getServerSession } from 'next-auth'
 
+interface Genres {
+	genres: string[]
+}
+
 const colors = [
-	'#D70060',
-	'#E54028',
-	'#F18D05',
-	'#f2bc06',
-	'#5EB11C',
-	'#3A7634',
-	'#0ABEBE',
-	'#00A1CB',
-	'#115793',
+	'#f72585',
+	'#b5179e',
+	'#7209b7',
+	'#560bad',
+	'#480ca8',
+	'#3a0ca3',
+	'#3f37c9',
+	'#4361ee',
+	'#4895ef',
+	'#4cc9f0',
 ]
-const mockup = [
-	{
-		color: colors[0],
-		title: 'Alternative',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[1],
-		title: 'Blues',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[2],
-		title: 'Classical',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[3],
-		title: 'Country',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[4],
-		title: 'Dance',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[5],
-		title: 'Electronic',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[6],
-		title: 'Folk',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[7],
-		title: 'Funk',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-	{
-		color: colors[8],
-		title: 'Hip Hop',
-		inners: 7,
-		innerTitles: [
-			'Acoustic Blues',
-			'Blues Rock',
-			'Canadian Blues',
-			'Jazz Blues',
-			'Piano Blues',
-			'Soul Blues',
-			'Swamp Blues',
-		],
-		innerContent: ['hej', 'hej', 'hej', 'hej', 'hej', 'hej', 'hej'],
-	},
-]
+
+const doubleColors = [...colors, ...colors.reverse()]
 
 export default async function Categories() {
-	const session = await getServerSession()
+	const session = await getServerSession(authOptions)
+	const data = await getCategories()
 
-	async function getCategories(): Promise<Root | undefined> {
+	async function getCategories(): Promise<Genres | undefined> {
 		if (!session?.user.token) return
 		const res = await spotifyFetch(
-			'https://api.spotify.com/v1/browse/categories?limit=50',
+			'https://api.spotify.com/v1/recommendations/available-genre-seeds',
 			session.user.token
 		)
-
 		const data = await res.json()
 		return data
 	}
+
 	return (
 		<PageContent>
 			<GradientText>Categories</GradientText>
 			<div className="flex flex-col gap-4">
-				{mockup.map((item, index) => (
-					<CombinedAccordion
-						color={item.color}
-						title={item.title}
-						inners={item.inners}
-						innerTitles={item.innerTitles}
-						innerContent={item.innerContent}
-						key={index}
-					></CombinedAccordion>
-				))}
+				{data &&
+					data.genres.map((category, index) => (
+						<CategoriesAccordion
+							color={() => {
+								let colorIndex = index % doubleColors.length
+								return doubleColors[colorIndex]
+							}}
+							title={category}
+							key={index}
+							slug={category}
+						></CategoriesAccordion>
+					))}
 			</div>
 		</PageContent>
 	)
