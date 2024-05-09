365441a32f7804fb532a29cf7ac9c4dd69fe2aa0
Martin <martinbruun1995@gmail.com>
2024-05-09 23:27:59 +0200

    refactor: Update ImageCardWithOverlay component to handle optional text and subText props

--- src/components/cards/ImageCardWithOverlay.tsx
+++ src/components/cards/ImageCardWithOverlay.tsx
@@ -8,15 +8,21 @@ export default function ImageCardWithOverlay({
 }: {
 	imgSrc: string
 	imageAlt: string
-	text: string
-	subText: string
+	text?: string
+	subText?: string
 }) {
 	return (
 		<article className="min-h-[450px] w-full relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 max-w-sm mx-auto shadow-xl">
 			<Image className="rounded-md object-cover" src={imgSrc} alt={imageAlt} fill={true}></Image>
 			<div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-			<h3 className="z-10 text-3xl font-bold text-white">{text}</h3>
-			<div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">{subText}</div>
+			{text && subText ? (
+				<>
+					<h3 className="z-10 text-3xl font-bold text-white">{text}</h3>
+					<div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
+						{subText}
+					</div>
+				</>
+			) : null}
 		</article>
 	)
 }
