220c21a6272154060f0719c4a88bed40f3cde2ca
Martin <martinbruun1995@gmail.com>
2024-05-10 20:40:25 +0200

    refactor: Update next.config.mjs to enable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,7 @@ const nextConfig = {
 		},
 	},
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'i.scdn.co',
-				port: '',
-				pathname: '/image/**',
-			},
-		],
+		unoptimized: true,
 	},
 }
 
