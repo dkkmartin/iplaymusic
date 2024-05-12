05b4b42a2f164e15c76ac4e797743a1aab60d5c1
Martin <martinbruun1995@gmail.com>
2024-05-12 22:52:52 +0200

    refactor: Update POST route to handle error in fetching

--- src/app/api/search/[slug]/route.ts
+++ src/app/api/search/[slug]/route.ts
@@ -1,13 +1,19 @@
+import { error } from 'console'
+
 export const fetchCache = 'force-cache'
 export const revalidate = 3600
 
 export async function POST(request: Request, { params }: { params: { slug: string } }) {
 	const query = params.slug.toLowerCase()
 	const requestBody = await request.json()
+	let filter = requestBody.filter
+
 	if (!requestBody.token) {
 		return Response.json({ status: 400, text: 'No token provided' })
 	}
-	const res = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=playlist&limit=5`, {
+
+	if (!filter) filter = 'playlist'
+	const res = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=${filter}&limit=10`, {
 		headers: {
 			Authorization: `Bearer ${requestBody.token}`,
 		},
@@ -15,7 +21,7 @@ export async function POST(request: Request, { params }: { params: { slug: strin
 	if (!res.ok) {
 		const text = await res.text()
 		console.error(`Error in fetching ${query}: ${text}`)
-		return
+		return Response.json({ status: 400, message: `Error in fetching ${query}: ${text}` })
 	}
 	const data = await res.json()
 
