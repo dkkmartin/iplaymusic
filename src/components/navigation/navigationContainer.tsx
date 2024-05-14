ee80e202253f2c8b76135354d290e2968e258997
Martin <martinbruun1995@gmail.com>
2024-05-14 20:09:19 +0200

    refactor: Update navigation container to include music player and session authentication

--- src/components/navigation/navigationContainer.tsx
+++ src/components/navigation/navigationContainer.tsx
@@ -1,11 +1,19 @@
+import { authOptions } from '@/lib/authOptions'
 import NavigationBar from './navigationBar'
 import SettingsButton from './settings'
+import { getServerSession } from 'next-auth'
+import { WebPlayback } from '../player/musicPlayer'
 
-export default function NavigationContainer() {
+export default async function NavigationContainer() {
+	const session = await getServerSession(authOptions)
 	return (
-		<div className="grid grid-cols-10 items-center justify-center h-[65px] w-full fixed bottom-0 shadow-[0px_0px_20px_5px_#00000024] max-w-md bg-background dark:bg-[#111625]">
-			<NavigationBar className="col-start-1 col-end-9 max-w-[340px]"></NavigationBar>
-			<SettingsButton className="col-start-9 justify-self-center ml-8"></SettingsButton>
+		<div className="fixed bottom-0  w-full">
+			{session?.user.token ? <WebPlayback token={session?.user.token}></WebPlayback> : null}
+
+			<div className="grid grid-cols-10 items-center justify-center h-[65px] shadow-[0px_0px_20px_5px_#00000024] max-w-md bg-background dark:bg-[#111625]">
+				<NavigationBar className="col-start-1 col-end-9 max-w-[340px]"></NavigationBar>
+				<SettingsButton className="col-start-9 justify-self-center ml-8"></SettingsButton>
+			</div>
 		</div>
 	)
 }
