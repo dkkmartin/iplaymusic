dadef7a740cd5f6813474eb5cf0925321a623872
Martin <martinbruun1995@gmail.com>
2024-05-15 13:06:48 +0200

    refactor: Update User navigation component styling and remove unused imports

--- src/components/navigation/user.tsx
+++ src/components/navigation/user.tsx
@@ -4,12 +4,10 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
 	Dialog,
 	DialogContent,
-	DialogDescription,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog'
-import { Button } from '../ui/button'
 import { Separator } from '@/components/ui/separator'
 
 export default async function User() {
@@ -18,10 +16,8 @@ export default async function User() {
 	if (session) {
 		return (
 			<Dialog>
-				<DialogTrigger>
-					<Button className="w-full dark:bg-[#111625] dark:border-white" variant={'outline'}>
-						Profile
-					</Button>
+				<DialogTrigger className="w-full dark:bg-[#111625] dark:border-white border rounded py-2">
+					Profile
 				</DialogTrigger>
 				<DialogContent className="dark:bg-[#111625] dark:border-white w-11/12 rounded-lg">
 					<DialogHeader className="grid grid-cols-[1fr_2fr_1fr] justify-items-center items-center">
@@ -29,10 +25,8 @@ export default async function User() {
 							<AvatarImage src={session?.user?.image ?? ''} alt={session?.user?.name ?? ''} />
 							<AvatarFallback>{(session?.user?.name ?? '').slice(0, 1)}</AvatarFallback>{' '}
 						</Avatar>
-						<DialogTitle className="">
-							<h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight text-wrap">
-								{session?.user?.name}
-							</h1>
+						<DialogTitle className="scroll-m-20 text-2xl font-extrabold tracking-tight text-wrap">
+							{session?.user?.name}
 						</DialogTitle>
 					</DialogHeader>
 					<Separator className="dark:bg-white" />
