7fcc7f23742d0b7ab13d5495cad175de4cef1090
Martin <martinbruun1995@gmail.com>
2024-05-15 17:54:16 +0200

    refactor: Update stores.ts to include playback and device stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,11 +1,23 @@
 import { create } from 'zustand'
+import { Root as playbackRoot } from '@/types/spotify/playbackState'
+import { Root as deviceRoot } from '@/types/spotify/devices'
 
-type Store = {
-	firstTime: boolean
-	setFirstTime: (value: boolean) => void
+type playbackStore = {
+	playbackState: playbackRoot | undefined
+	setPlaybackState: (newState: playbackRoot) => void
 }
 
-export const firstTimeStore = create<Store>((set) => ({
-	firstTime: true,
-	setFirstTime: (value: boolean) => set({ firstTime: value }),
+export const usePlaybackStore = create<playbackStore>((set) => ({
+	playbackState: undefined,
+	setPlaybackState: (newState) => set(() => ({ playbackState: newState })),
+}))
+
+type deviceStore = {
+	devicesState: deviceRoot | undefined
+	setDevicesState: (newState: deviceRoot) => void
+}
+
+export const useDeviceStore = create<deviceStore>((set) => ({
+	devicesState: undefined,
+	setDevicesState: (newState) => set(() => ({ devicesState: newState })),
 }))
