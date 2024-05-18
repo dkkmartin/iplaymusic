2685f824877637371d6eacd1581b091371136af3
Martin <martinbruun1995@gmail.com>
2024-05-18 18:37:23 +0200

    refactor: Update recentlyPlayed.ts types for Spotify API changes

--- src/types/spotify/recentlyPlayed.ts
+++ src/types/spotify/recentlyPlayed.ts
@@ -1,9 +1,15 @@
 export interface Root {
-	items: Item[]
+	href: string
+	limit: number
 	next: string
 	cursors: Cursors
-	limit: number
-	href: string
+	total: number
+	items: Item[]
+}
+
+export interface Cursors {
+	after: string
+	before: string
 }
 
 export interface Item {
@@ -23,59 +29,71 @@ export interface Track {
 	external_urls: ExternalUrls4
 	href: string
 	id: string
-	is_local: boolean
+	is_playable: boolean
+	linked_from: LinkedFrom
+	restrictions: Restrictions2
 	name: string
 	popularity: number
 	preview_url: string
 	track_number: number
 	type: string
 	uri: string
+	is_local: boolean
 }
 
 export interface Album {
 	album_type: string
-	artists: Artist[]
+	total_tracks: number
 	available_markets: string[]
-	external_urls: ExternalUrls2
+	external_urls: ExternalUrls
 	href: string
 	id: string
 	images: Image[]
 	name: string
 	release_date: string
 	release_date_precision: string
-	total_tracks: number
+	restrictions: Restrictions
 	type: string
 	uri: string
-}
-
-export interface Artist {
-	external_urls: ExternalUrls
-	href: string
-	id: string
-	name: string
-	type: string
-	uri: string
+	artists: Artist[]
 }
 
 export interface ExternalUrls {
 	spotify: string
 }
 
+export interface Image {
+	url: string
+	height: number
+	width: number
+}
+
+export interface Restrictions {
+	reason: string
+}
+
+export interface Artist {
+	external_urls: ExternalUrls2
+	href: string
+	id: string
+	name: string
+	type: string
+	uri: string
+}
+
 export interface ExternalUrls2 {
 	spotify: string
 }
 
-export interface Image {
-	height: number
-	url: string
-	width: number
-}
-
 export interface Artist2 {
 	external_urls: ExternalUrls3
+	followers: Followers
+	genres: string[]
 	href: string
 	id: string
+	images: Image2[]
 	name: string
+	popularity: number
 	type: string
 	uri: string
 }
@@ -84,12 +102,31 @@ export interface ExternalUrls3 {
 	spotify: string
 }
 
+export interface Followers {
+	href: string
+	total: number
+}
+
+export interface Image2 {
+	url: string
+	height: number
+	width: number
+}
+
 export interface ExternalIds {
 	isrc: string
+	ean: string
+	upc: string
 }
 
 export interface ExternalUrls4 {
 	spotify: string
+}
+
+export interface LinkedFrom {}
+
+export interface Restrictions2 {
+	reason: string
 }
 
 export interface Context {
@@ -101,9 +138,4 @@ export interface Context {
 
 export interface ExternalUrls5 {
 	spotify: string
-}
-
-export interface Cursors {
-	after: string
-	before: string
 }
