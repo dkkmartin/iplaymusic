f6d6865db8659cca5de32d94a69e419888e118d9
Martin <martinbruun1995@gmail.com>
2024-05-19 17:21:43 +0200

    refactor: Update headerNavigation component to handle additional route modifications

--- src/components/header/headerNavigation.tsx
+++ src/components/header/headerNavigation.tsx
@@ -8,7 +8,15 @@ import Link from 'next/link'
 export default function HeaderNavigation() {
 	const router = useRouter()
 	const pathname = usePathname()
-	const modiefiedPathname = pathname === '/' ? 'featured' : pathname.slice(1)
+	let modiefiedPathname
+
+	if (pathname === '/') {
+		modiefiedPathname = 'featured'
+	} else if (pathname.startsWith('/artist')) {
+		modiefiedPathname = 'artist'
+	} else {
+		modiefiedPathname = pathname.slice(1)
+	}
 
 	return (
 		<header className="grid grid-cols-3 items-center sticky top-2 z-10 m-auto rounded-full h-10 w-11/12 px-4 backdrop-blur-lg">
