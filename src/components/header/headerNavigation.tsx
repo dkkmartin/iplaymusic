3745dd3b0ab16c7259c310f1afa9001e8db81235
Martin <martinbruun1995@gmail.com>
2024-05-19 18:58:17 +0200

    refactor: Update headerNavigation component to handle additional route modifications

--- src/components/header/headerNavigation.tsx
+++ src/components/header/headerNavigation.tsx
@@ -19,7 +19,11 @@ export default function HeaderNavigation() {
 	}
 
 	return (
-		<header className="grid grid-cols-3 items-center sticky top-2 z-10 m-auto rounded-full h-10 w-11/12 px-4 backdrop-blur-lg">
+		<header
+			className={`grid grid-cols-3 items-center z-10 rounded-full h-10 w-11/12 px-4 backdrop-blur-lg ${
+				pathname.startsWith('/artist') ? 'fixed m-auto left-0 right-0 top-2' : 'sticky top-2 m-auto'
+			}`}
+		>
 			<Button
 				aria-label="Go back"
 				className="justify-self-start"
