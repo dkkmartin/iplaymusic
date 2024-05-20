120427bb96d7fc57cde11401904bcc5b6141c0e4
Martin <martinbruun1995@gmail.com>
2024-05-20 18:57:14 +0200

    refactor: Moved msToTime function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,9 @@ export const imageLoader = ({ src }: { src: string }) => {
 }
 
 export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export const msToTime = (duration: number) => {
+	const minutes = Math.floor(duration / 60000)
+	const seconds = Number(((duration % 60000) / 1000).toFixed(0))
+	return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+}
