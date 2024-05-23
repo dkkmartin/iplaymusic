09bdc0b024772a8cd773fa63d2da639844316ace
Martin <martinbruun1995@gmail.com>
2024-05-23 11:20:53 +0200

    refactor: Update featured playlists page with lazy loading for images

--- src/app/(authenticated)/featured-playlists/page.tsx
+++ src/app/(authenticated)/featured-playlists/page.tsx
@@ -1,7 +1,10 @@
+import PageContent from '@/components/pages/pageContent'
+import GradientText from '@/components/text/gradientHeading'
 import { authOptions } from '@/lib/authOptions'
 import { Root } from '@/types/featured-playlist/playlist'
 import { getServerSession } from 'next-auth'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function FeaturedPlaylists() {
 	const session = await getServerSession(authOptions)
@@ -23,19 +26,26 @@ export default async function FeaturedPlaylists() {
 
 	return (
 		playlistData?.playlists?.items && (
-			<section>
-				{playlistData?.playlists?.items.map((playlist: any, index: number) => (
-					<div key={index}>
-						<Image
-							src={playlist.images[0].url}
-							width={300}
-							height={300}
-							alt={`${playlist.name} cover`}
-						/>
-						<h1>{playlist.name}</h1>
-					</div>
-				))}
-			</section>
+			<PageContent>
+				<GradientText headingSize="h1">Featured</GradientText>
+				<section className="grid grid-cols-2 gap-4">
+					{playlistData?.playlists?.items.map((playlist: any, index: number) => (
+						<Link href={`/playlist/${playlist.id}`} key={index}>
+							<div key={index}>
+								<Image
+									src={playlist.images[0].url}
+									width={200}
+									height={200}
+									alt={`${playlist.name} cover`}
+								/>
+								<h1 className="scroll-m-20 py-4 text-2xl font-semibold tracking-tight">
+									{playlist.name}
+								</h1>
+							</div>
+						</Link>
+					))}
+				</section>
+			</PageContent>
 		)
 	)
 }
