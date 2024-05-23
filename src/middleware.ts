6348c8e06a7a5bd5bcc60cf6503facc905720d38
Martin <martinbruun1995@gmail.com>
2024-05-23 11:20:49 +0200

    refactor: Update middleware.ts to include '/featured-playlists' in the matcher array

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/', '/categories', '/playlist', '/search', '/artist'] }
+export const config = {
+	matcher: ['/', '/categories', '/playlist', '/search', '/artist', '/featured-playlists'],
+}
