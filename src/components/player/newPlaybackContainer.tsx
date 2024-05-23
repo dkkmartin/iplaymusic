0270660c402259afb19698a761421d978e1d089a
Martin <martinbruun1995@gmail.com>
2024-05-23 11:52:34 +0200

    refactor: Improve track name detection in NewPlaybackContainer

--- src/components/player/newPlaybackContainer.tsx
+++ src/components/player/newPlaybackContainer.tsx
@@ -7,6 +7,7 @@ import {
 } from '@/lib/spotify/utils'
 import { useCurrentDeviceStore, usePlaybackStore } from '@/lib/stores'
 import { cn } from '@/lib/utils'
+import React from 'react'
 
 interface NewPlaybackProps {
 	children: React.ReactNode
@@ -55,13 +56,39 @@ export default function NewPlaybackContainer({
 		}
 	}
 
+	// Recursive function to check if a given element or its children contain the track name
+	// Spotfiy API for playback state doesn't specify which track number is playing in a playlist
+	// So we have to check if the track name is contained in the children
+	function containsTrackName(element: React.ReactNode): boolean {
+		if (React.isValidElement(element)) {
+			if (
+				typeof element.props.children === 'string' &&
+				element.props.children.includes(playbackState?.item?.name)
+			) {
+				return true
+			}
+
+			if (Array.isArray(element.props.children)) {
+				return element.props.children.some(containsTrackName)
+			}
+
+			if (React.isValidElement(element.props.children)) {
+				return containsTrackName(element.props.children)
+			}
+		}
+		return false
+	}
+
+	// Check if children contains the name of the currently playing track
+	const isPlaying = React.Children.toArray(children).some(containsTrackName)
+
 	// Single track playback
 	// If the playback state uri is the same as the uri prop, color it green
 	return !playbackState ? (
 		<div onClick={handleClick} className={className}>
 			{children}
 		</div>
-	) : playbackState.item?.uri === uri ? (
+	) : playbackState.item?.uri === uri || isPlaying ? (
 		<div onClick={handleClick} className={cn('text-green-600', className)}>
 			{children}
 		</div>
