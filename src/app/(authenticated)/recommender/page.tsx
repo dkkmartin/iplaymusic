975983f9d19924030a7533300d4a279f28196b4e
Martin <martinbruun1995@gmail.com>
2024-05-24 13:46:32 +0200

    refactor: Update navigation link in NavigationBar to point to /recommender page

--- src/app/(authenticated)/recommender/page.tsx
+++ src/app/(authenticated)/recommender/page.tsx
@@ -1,5 +1,16 @@
-import { createGoogleGenerativeAI } from '@ai-sdk/google'
-
 export default async function Recommender() {
+	const response = await fetch(process.env.NEXTAUTH_URL + '/api/recommend', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ userPreferences: ['pop', 'vocals', 'bass', 'danceable', 'high-bpm'] }),
+	})
+
+	console.log(response)
+	//const data = await response.json()
+
+	//console.log(data)
+
 	return <p>hej</p>
 }
