aa596d207564c6fb35d0b34126a3978d637bdd0a
Martin <martinbruun1995@gmail.com>
2024-05-25 01:22:17 +0200

    refactor: Add ref prop to PageContent component

--- src/components/pages/pageContent.tsx
+++ src/components/pages/pageContent.tsx
@@ -4,8 +4,9 @@ import { ReactNode } from 'react'
 interface PageContentProps {
 	children: ReactNode
 	className?: string
+	ref?: React.Ref<HTMLDivElement>
 }
 
-export default function PageContent({ children, className }: PageContentProps) {
+export default function PageContent({ children, className, ref }: PageContentProps) {
 	return <section className={cn('p-6 mb-32', className)}>{children}</section>
 }
