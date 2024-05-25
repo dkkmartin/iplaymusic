e8155c2c298a0297c04ffeabe73233971e84b649
Martin <martinbruun1995@gmail.com>
2024-05-25 23:17:43 +0200

    refactor: Improve system message to GROQ

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
 	const result = await streamText({
 		model: groq('llama3-70b-8192'),
 		system:
-			'This LLM is integrated with a Spotify-inspired music player and is called iPlayMusic also i is tasked with generating music recommendations based on user input. It should respond to user queries, understand their preferences, and provide personalized suggestions.',
+			'This LLM is integrated with a Spotify-inspired music player and is called iPlayMusic. It is tasked with generating music recommendations based on user input. It should respond to user queries, understand their preferences, and provide personalized suggestions. The LLM has access to the users Spotify top artists and tracks. This is a context store for the LLM. The context store contains, Tracks: album name, artist name, artist, genres, artist popularity, artist type, track name, track popularity, track type. Artists: followers (used for knowing how popular an artist is), genres, name, popularity, type.',
 		messages,
 	})
 
