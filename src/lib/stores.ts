186a0816b2f35696d02a355475be9a34dddb0851
Martin <martinbruun1995@gmail.com>
2024-05-25 23:04:15 +0200

    refactor: Add user top items store for top tracks and artists

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand'
 import { Root as playbackRoot } from '@/types/player/playbackState'
 import { Root as deviceRoot } from '@/types/player/devices'
+import { Root as userTopArtistsRoot } from '@/types/user/topArtists'
+import { Root as userTopTracksRoot } from '@/types/user/topTracks'
 
 type playbackStore = {
 	playbackState: playbackRoot | undefined
@@ -30,4 +32,34 @@ type currentDeviceStore = {
 export const useCurrentDeviceStore = create<currentDeviceStore>((set) => ({
 	currentDevice: undefined,
 	setCurrentDevice: (newState) => set(() => ({ currentDevice: newState })),
+}))
+
+type userTopItemsStore = {
+	topTracks: userTopTracksRoot | undefined
+	setTopTracks: (newState: userTopTracksRoot) => void
+	topArtists: userTopArtistsRoot | undefined
+	setTopArtists: (newState: userTopArtistsRoot) => void
+}
+
+export const useUserTopItemsStore = create<userTopItemsStore>((set) => ({
+	topTracks: undefined,
+	setTopTracks: (newState) => set(() => ({ topTracks: newState })),
+	topArtists: undefined,
+	setTopArtists: (newState) => set(() => ({ topArtists: newState })),
+}))
+
+type Message = {
+	id: string
+	role: string
+	content: string
+}
+
+type chatStore = {
+	chatMessages: Message[] | undefined
+	setChatMessages: (newState: Message[]) => void
+}
+
+export const useChatStore = create<chatStore>((set) => ({
+	chatMessages: undefined,
+	setChatMessages: (newState) => set(() => ({ chatMessages: newState })),
 }))
