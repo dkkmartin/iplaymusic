f035106c1ac6cdbea34cca8c13531da5ca4fc160
Martin <martinbruun1995@gmail.com>
2024-05-26 00:34:20 +0200

    refactor: Update chat component with user context and markdown support

--- src/app/(authenticated)/recommender/page.tsx
+++ src/app/(authenticated)/recommender/page.tsx
@@ -3,11 +3,19 @@
 import PageContent from '@/components/pages/pageContent'
 import { Input } from '@/components/ui/input'
 import { useChatStore } from '@/lib/stores'
+import { useUserContext } from '@/lib/useUserContext'
 import { useChat } from 'ai/react'
+import { useSession } from 'next-auth/react'
 import { useEffect } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 export default function Chat() {
-	const { messages, input, handleInputChange, handleSubmit, setMessages, isLoading } = useChat()
+	const { data: session, status } = useSession()
+	const { userContext } = useUserContext(session?.user.token ?? '')
+	const { messages, input, handleInputChange, handleSubmit, setMessages, isLoading, append } =
+		useChat({
+			body: { context: userContext },
+		})
 	const chatMessagesStore = useChatStore((state) => state.chatMessages)
 	const setChatMessagesStore = useChatStore((state) => state.setChatMessages)
 
@@ -32,15 +40,18 @@ export default function Chat() {
 				<div className="overflow-scroll h-full pb-4">
 					{messages.map((m) => (
 						<div key={m.id} className={'chat ' + (m.role === 'user' ? ' chat-start' : 'chat-end')}>
-							<div className="chat-bubble dark:bg-[#111625] text-white bg-black">{m.content}</div>
+							<div className="chat-bubble dark:bg-[#111625] text-white bg-black">
+								<ReactMarkdown>{m.content}</ReactMarkdown>
+							</div>
 						</div>
 					))}
 				</div>
 				<form onSubmit={handleSubmit}>
 					<Input
+						disabled={isLoading}
 						className="p-2 mb-8 border dark:border-gray-300 border-black rounded shadow-xl"
 						value={input}
-						placeholder="Say something..."
+						placeholder={isLoading ? 'Loading...' : 'Say something...'}
 						onChange={handleInputChange}
 					/>
 				</form>
