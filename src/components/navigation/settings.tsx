8b8cde576c22a4c20360d7c80f77839bb5978049
Martin <martinbruun1995@gmail.com>
2024-06-02 02:12:55 +0200

    refactor: Add "Profile" button to settings page

--- src/components/navigation/settings.tsx
+++ src/components/navigation/settings.tsx
@@ -1,8 +1,15 @@
 import { ThemeSwitcher } from './theme-switcher'
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+	SheetClose,
+} from '@/components/ui/sheet'
 import { Separator } from '@/components/ui/separator'
 import IonSettingsSharp from '../svg/IonSettingsSharp'
-import User from './user'
+
 import { Button } from '../ui/button'
 import Link from 'next/link'
 
@@ -18,7 +25,13 @@ export default function SettingsButton({ className }: { className?: string }) {
 					<Separator className="dark:bg-white" />
 				</SheetHeader>
 				<ThemeSwitcher></ThemeSwitcher>
-				<User></User>
+				<SheetClose asChild>
+					<Link href={'/profile'}>
+						<Button className="w-full dark:bg-[#111625] dark:border-white" variant={'outline'}>
+							Profile
+						</Button>
+					</Link>
+				</SheetClose>
 				<Link href="/api/auth/signout">
 					<Button className="dark:bg-[#111625] border-red-700 w-full" variant={'outline'}>
 						Logout
